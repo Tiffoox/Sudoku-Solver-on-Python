d27099cf555c998cb8c71dd922913a12efbf3d92
Tiffoox <quentin.dij@hotmail.fr>
2020-04-12 20:19:46 +0200

    Assembly all the squares in order to get a grid

--- grid.py
+++ grid.py
@@ -1,15 +1,25 @@
-#Give first grid values
+import numpy as np
 
-blank = ""
+def grid():
+    #Give first grid values
 
-top_left = ((5, 3, blank), (6, blank, blank), (blank, 9, 8))
-top_center = ((blank, 7, blank), (1, 9, 5), (blank, blank, blank))
-top_right = ((blank, blank, blank), (blank, blank, blank), (blank, 6, blank))
+    blank = np.NaN
 
-middle_left = ((8, blank, blank), (4, blank, blank), (7, blank, blank))
-middle_center = ((blank, 6, blank), (8, blank, 3), (blank, 2, blank))
-middle_right = ((blank, blank, 3), (blank, blank, 1), (blank, blank, 6))
+    top_left = ((5, 3, blank), (6, blank, blank), (blank, 9, 8))
+    top_center = ((blank, 7, blank), (1, 9, 5), (blank, blank, blank))
+    top_right = ((blank, blank, blank), (blank, blank, blank), (blank, 6, blank))
 
-bottom_left = ((blank, 6, blank), (blank, blank, blank), (blank, blank, blank))
-bottom_center = ((blank, blank, blank), (4, 1, 9), (blank, 8, blank))
-bottom_right = ((2, 8, blank), (blank, blank, 5), (blank, 7, 9))
+    middle_left = ((8, blank, blank), (4, blank, blank), (7, blank, blank))
+    middle_center = ((blank, 6, blank), (8, blank, 3), (blank, 2, blank))
+    middle_right = ((blank, blank, 3), (blank, blank, 1), (blank, blank, 6))
+
+    bottom_left = ((blank, 6, blank), (blank, blank, blank), (blank, blank, blank))
+    bottom_center = ((blank, blank, blank), (4, 1, 9), (blank, 8, blank))
+    bottom_right = ((2, 8, blank), (blank, blank, 5), (blank, 7, 9))
+
+
+    left = top_left + middle_left + bottom_left
+    center = top_center + middle_center + bottom_center
+    right = top_right + middle_right + bottom_right
+    full_grid = np.c_[left, center, right]
+    return full_grid
---
+++ main.py
@@ -0,0 +1,5 @@
+from grid import grid
+
+
+x = grid()
+
