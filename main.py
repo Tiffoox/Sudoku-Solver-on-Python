94487117838369c05a2b3cc47f5790f535cbb22f
Tiffoox <quentin.dij@hotmail.fr>
2020-04-12 21:47:02 +0200

    Adapt the square function for numpy array

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from grid import grid
 from square import square
 import numpy as np
-
 grid = grid()
-
+x = []
 for index_row in range(9):
     for index_column in range(9):
+        #find the corresponding row, column and square of the cell
         row = list(grid[index_row, :])
         column = list(grid[:, index_column])
-        square = grid[0:3, 0:3]
-        square = list(np.array(square).flatten())
+        current_square = square(grid, [index_row, index_column])
+        x.append(current_square)
 
+        #return the value of the square in a list
+        current_square = list(np.array(current_square).flatten())
 
         grid_cell = grid[index_row, index_column]
         possible_numbers = []
         for number in range(9):
             if grid_cell == "":
-                if grid_cell not in row and grid_cell not in column and grid_cell not in square:
+                if grid_cell not in row and grid_cell not in column and grid_cell not in current_square:
                     possible_numbers.append(number)
+
         if len(possible_numbers) == 1:
             grid[index_row, index_column] = possible_numbers[0]
--- square.py
+++ square.py
@@ -4,18 +4,28 @@ def square(grid, cell):
     :param cell: Interested cell
     :return: the square of this cels
     """
-    appropriate_rows = find_approapriate_vector(cell[0])
-    appropriate_columns = find_approapriate_vector(cell[1])
-    square = grid[appropriate_rows, appropriate_columns]
+    selected_rows = find_approapriate_rows(grid, cell[0])
+    square = find_approapriate_columns(selected_rows, cell[1])
     return square
 
-def find_approapriate_vector(value):
-    first = [0, 1 ,2]
+def find_approapriate_rows(grid, row):
+    first = [0, 1, 2]
     second = [3, 4, 5]
     third = [6, 7, 8]
-    if value in first:
-        return first
-    elif value in second:
-        return second
+    if row in first:
+        return grid[0:3]
+    elif row in second:
+        return grid[4:6]
     else:
-        return third
+        return grid[7:9]
+
+def find_approapriate_columns(selected_rows, column):
+    first = [0, 1, 2]
+    second = [3, 4, 5]
+    third = [6, 7, 8]
+    if column in first:
+        return selected_rows[:, 0:3]
+    elif column in second:
+        return selected_rows[:, 4:6]
+    else:
+        return selected_rows[:, 7:9]
