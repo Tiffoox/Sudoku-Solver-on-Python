dbd87bf4f357194bcf3913d7ae507a195c4c31a9
Tiffoox <quentin.dij@hotmail.fr>
2020-04-18 00:06:26 +0200

    User in the navbar / glyphicons / static files

---
+++ mysite/register/views.py
@@ -0,0 +1,19 @@
+# views.py
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
+
+# Create your views here.
+def register(response):
+
+        form = RegisterForm(response.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("/home")
+        else:
+            form = RegisterForm()
+
+        return render(response, "register/register.html", {"form":form})
