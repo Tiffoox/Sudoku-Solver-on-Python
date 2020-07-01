09c725933a335619de76b850658d6db412ed12a4
Tiffoox <quentin.dij@hotmail.fr>
2020-07-01 10:52:58 +0200

    Last commit

--- mysite/main/maker_and_solver/maker.py
+++ mysite/main/maker_and_solver/maker.py
@@ -3,7 +3,7 @@ from sudoku import Sudoku
 import numpy as np
 # Initializes a Sudoku puzzle with 3 x 3 sub-grid and
 # generates a puzzle with half of the cells empty
-
+#small changes
 def maker():
     puzzle = Sudoku(3).difficulty(0.5)
     grid = np.array(puzzle.board)
